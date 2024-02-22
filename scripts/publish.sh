15dbaec0784c3f7cb9581502bfa9ff86be011217
Sam Goodwin <sam@packyak.ai>
2024-02-21 21:52:14 -0800

    chore: set up publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,17 +1,21 @@
 #! /usr/bin/env bash
 
+set -e
+
+extra_args=$*
 version=$(node -p "require('./package.json').version")
+
+pnpm run clean
+pnpm run build
 
 script_dir=$(dirname -- "${BASH_SOURCE[0]}")
 echo "Script directory: $script_dir"
 
-node $script_dir/bump.mjs
+node $script_dir/bump.mjs ${extra_args}
 
-echo $version
-
-# poetry build && poetry publish
-# cd packyak-aws-cdk
-# pnpm run build
-# pnpm run package
-# pnpm run publish:pypi
-# npm publish
+poetry build && poetry publish
+cd packyak-aws-cdk
+pnpm run build
+pnpm run package
+pnpm run publish:pypi
+npm publish
