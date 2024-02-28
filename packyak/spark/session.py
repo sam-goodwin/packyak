d6f032673a790c99ea104730543d3c28810eef01
Sam Goodwin <sam@packyak.ai>
2024-02-27 23:53:04 -0800

    fix: import pyspark.sql after findspark.init()

--- packyak/spark/session.py
+++ packyak/spark/session.py
@@ -1,23 +1,32 @@
 import os
+from typing import cast
 
-def init_session(
-    cwd: str = os.getcwd(),
-    venv: str = None
-):
+
+def init_session(cwd: str = os.getcwd(), venv: str | None = None):
     return session_builder(cwd, venv).getOrCreate()
 
 
-def session_builder(
-    cwd: str = os.getcwd(),
-    venv: str = None
-):
-    venv = venv if venv is not None else os.path.join(cwd, ".venv", "bin", "python")
-    
+is_spark_found = False
+
+
+def session_builder(cwd: str = os.getcwd(), venv: str | None = None):
+    global is_spark_found
+    if not is_spark_found:
+        is_spark_found = True
+        import findspark
+
+        findspark.init()
+
     from pyspark.sql import SparkSession
-    
-    return (
-        SparkSession.builder.master("yarn")
-        .config("spark.pyspark.python", venv)
-        .config("spark.pyspark.driver.python", venv)
-        .config("spark.pyspark.virtualenv.enabled", "false")
+
+    venv = venv if venv is not None else os.path.join(cwd, ".venv", "bin", "python")
+
+    return cast(
+        SparkSession.Builder,
+        (
+            SparkSession.builder.master("yarn")
+            .config("spark.pyspark.python", venv)
+            .config("spark.pyspark.driver.python", venv)
+            .config("spark.pyspark.virtualenv.enabled", "false")
+        ),
     )
