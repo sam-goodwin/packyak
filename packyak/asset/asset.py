0d86db7fbf1b643f579fda75045704bc7207b1ae
Sam Goodwin <sam@packyak.ai>
2024-03-07 13:40:51 -0800

    improvements

--- packyak/asset/asset.py
+++ packyak/asset/asset.py
@@ -4,6 +4,7 @@ import dask.array as da
 from dask.base import DaskMethodsMixin
 import dask.dataframe as dd
 import pyspark as sp
+import pyspark.sql as sql
 import pyspark.pandas as ps
 import pandas as pd
 import numpy as np
@@ -15,6 +16,7 @@ from dataclasses import dataclass
 from typing import (
     Any,
     Callable,
+    Coroutine,
     Generic,
     Literal,
     NamedTuple,
@@ -32,7 +34,7 @@ from packyak.storage.folder import Folder
 Location = str | Folder | Bucket | None
 
 
-Frame = da.Array | dd.DataFrame | ps.DataFrame[Any]
+Frame = da.Array | dd.DataFrame | ps.DataFrame[Any] | sql.DataFrame
 
 
 class Format(ABC):
@@ -42,11 +44,9 @@ class Format(ABC):
 
 
 class ZarrFormat(Format):
-    kwargs: dict[str, Any] = {}
-
     def __init__(
         self,
-        partition: "Any | None" = None,
+        subject: "Any | None" = None,
         component: Optional[str] = None,
         storage_options: Optional[dict[str, Any]] = None,
         overwrite: bool = False,
@@ -55,16 +55,8 @@ class ZarrFormat(Format):
         return_stored: bool = False,
         **kwargs: dict[str, Any],
     ):
-        self.kwargs = {
-            "component": component,
-            "storage_options": storage_options,
-            "overwrite": overwrite,
-            "region": region,
-            "chunks": chunks,
-            "return_stored": return_stored,
-            **kwargs,
-        }
-        self.partition = partition
+        self.kwargs = kwargs
+        self.subject = subject
         self.component = component
         self.storage_options = storage_options
         self.overwrite = overwrite
@@ -73,12 +65,23 @@ class ZarrFormat(Format):
         self.return_stored = return_stored
 
     def save_dask_array(self, location: str, frame: da.Array) -> da.Array:
-        return frame.to_zarr(location, **self.kwargs)
+        return frame.to_zarr(
+            location,
+            **{
+                "component": self.component,
+                "storage_options": self.storage_options,
+                "overwrite": self.overwrite,
+                "region": self.region,
+                "chunks": self.chunks,
+                "return_stored": self.return_stored,
+                **self.kwargs,
+            },
+        )
 
 
 def zarr(
     location: Location,
-    partition: "Any | None" = None,
+    subject: "Any | None" = None,
     component: Optional[str] = None,
     storage_options: Optional[dict[str, Any]] = None,
     overwrite: bool = False,
@@ -88,7 +91,7 @@ def zarr(
     **kwargs: dict[str, Any],
 ):
     return artifact(
-        partition=partition,
+        subject=subject,
         location=location,
         format=ZarrFormat(
             component=component,
@@ -107,13 +110,10 @@ class Artifact:
         self.func = func
 
 
-A = TypeVar("A", bound=Artifact)
-
-
 def artifact(
     format: Format | None = None,
     location: Location = None,
-    partition: "Any | None" = None,
+    subject: "Any | None" = None,
     deps: "list[Artifact | Table[TableSchema]] | None" = None,
 ):
     def decorate(func: Callable[..., da.Array | npt.ArrayLike]) -> Artifact:
@@ -134,14 +134,16 @@ DaskValue = DaskMethodsMixin
 
 
 def table(
-    location: Location,
     *,
-    partition: "Any | None" = None,
+    location: Location = None,
+    subject: "Any | None" = None,
     format: Literal["parquet"] | Literal["orc"] | Literal["json"] = "parquet",
     source: bool | None = None,
     deps: list[Artifact | Table[TableSchema]] | None = None,
 ):
-    def decorate(func: Callable[..., DaskValue | ps.DataFrame[TableSchema]]):
+    def decorate(
+        func: Callable[..., DaskValue | ps.DataFrame[TableSchema]],
+    ):
         return Table[Any](func)  # type: ignore
 
     return decorate
@@ -174,20 +176,28 @@ def PartitionKey(dtype: type[PK]) -> PK:
     return PartitionKeySpec(dtype)  # type: ignore
 
 
-class Manifest(NamedTuple):
-    pass
+class Thing(NamedTuple):
+    @classmethod
+    def has_a(cls, object: type["Thing"]):
+        return artifact()
+
+    @classmethod
+    def has_an(cls, object: type["Thing"]):
+        return artifact()
+
+    @classmethod
+    def has_many(cls, object: type["Thing"]):
+        return artifact()
 
 
 # Example:
 
 scan_bucket = Bucket("scan_bucket")
 
-pheno_scans = scan_bucket / "pheno_scans"
-
-procode_scans = scan_bucket / "procode_scans"
+orion_scans = scan_bucket / "orion" / "scans"
 
 
-class Scan(Manifest):
+class Scan(Thing):
     # define partition keys on the Model explicitly
     scan_id = PartitionKey(str)
     # e.g. second PK
@@ -197,44 +207,95 @@ class Scan(Manifest):
 
 
 # bootstrap the Scan graph by defining a function that returns the initial table
-@table(location=(scan_bucket / "scans"), partition=Scan, source=True)
-async def scan_images(
+@table(subject=Scan, source=True)
+def orion_scan(
+    sc: sp.SparkContext,
+    spark: sql.SparkSession,
     # a reference to "this" table, i.e the current version of the table (at the time we branched)
     self: ps.DataFrame[Scan] | None,
 ) -> ps.DataFrame[Scan]:
     # scan the s3 directory, compare with previous_scans, return the updated table
+
+    # option 1 - use raw boto client
+    import boto3
+
+    s3 = boto3.client("s3")
+
+    s3.list_objects_v2(...)
+
+    # option 2 - use the PackYak bucket abstraction
+
+    orion_scans.list_sync()
+
+    # option 3 - use Spark Context
+
+    sc.binaryFiles()
+
+    # option 4 - use SparkSession
+
+    spark.read.json()
+    spark.read.parquet()
+    spark.sql("select ...")
+
     return ps.DataFrame()
 
 
+class Image(Scan):
+    image_id = PartitionKey(str)
+
+
+# could model dependencies explicitly
+@Scan.has_an(Image)
+def orion_image() -> da.Array:
+    return da.empty()
+
+
+class Lobe(Thing):
+    scan_id: str
+    # partition using lobe_id for anything that depends on a lobe
+    lobe_id = PartitionKey(str)
+
+
+@Scan.has_many(Lobe)
+def lobes() -> da.Array:
+    return da.empty()
+
+
 @artifact(
-    partition=Scan,
+    subject=Scan,
     # explicit hard-coded string (escape hatch from Bucket / Folder abstraction)
     location="s3://bucket/phenotype/zarrs/",
     # explicit format is required when using low level @artifact API
     format=ZarrFormat(chunks=(1, 1, 1, -1, -1)),
     # explicit dependencies
-    deps=[scan_images],
+    deps=[orion_scan],
 )
 def pheno_image_zarr(scan: Scan) -> npt.ArrayLike:
     return run_artemis(scan)
 
 
 # @zarr is a wrapper around @artifact optimized for defining artifacts with the ZarrFormat
-# this is equivalent to the above ^ but captures the intent better
+# captures the intent better than @artifact
 @zarr(
+    subject=Scan,
     location=(scan_bucket / "scans" / Scan.scan_id / "procode"),
-    partition=Scan,
     # is this necessary? I can return a pre-chunked array
     chunks=(1, 1, 1, -1, -1),
 )
-def procode_image_zarr(scan_images: dd.DataFrame) -> da.Array:
-    pheno_scans = scan_images.where(scan_images.s3_dir == "procode")
+# this is a different take on the above ^ that uses a dask data frame
+# to work with the scan_image table instead of an individual Scan record
+# it then uses map_blocks to fan out
+# it showcases how easy it is to pivot perspective declaratively with types
+def procode_image_zarr(
+    scan_images: dd.DataFrame,
+) -> dd.DataFrame:  # TODO: can return type be anything other than a data frame?
+    pheno_scans = scan_images.where(scan_images.scan_type == "procode")
     return pheno_scans.map_blocks(run_artemis)
 
 
 @artifact(
-    location=(scan_bucket / "aligned_lobes"),
-    partition=Scan,
+    location=(scan_bucket / "aligned_lobes" / Scan.scan_id),
+    subject=Scan,
     # explicit dependencies are supported
     deps=[pheno_image_zarr, procode_image_zarr],
 )
