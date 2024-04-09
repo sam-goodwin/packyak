89d5b9115a5a7a06d9f12c83e4527a9cda75a779
Sam Goodwin <sam@packyak.ai>
2024-04-08 20:28:29 -0700

    fix: use sudo when writing to /etc/profile.d

--- packyak-aws-cdk/scripts/write-env-variables.sh
+++ packyak-aws-cdk/scripts/write-env-variables.sh
@@ -2,30 +2,25 @@
 
 ENV_FILE=/etc/profile.d/packyak.sh
 
-touch $ENV_FILE
-
-# Create the /mnt/shared directory with 755 permissions
-chmod +x $ENV_FILE
-
-# This makes new files have 644 and new directories have 755 permissions by default
-umask 022
+sudo touch $ENV_FILE
 
 # Create the ENV_FILE file with explicit permissions
-touch $ENV_FILE
-chmod 644 $ENV_FILE
-chown root:root $ENV_FILE
+sudo touch $ENV_FILE
+sudo chmod 644 $ENV_FILE
+sudo chown root:root $ENV_FILE
+sudo chmod +x $ENV_FILE
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --*=*) 
             key=$(echo $1 | sed 's/--//;s/=.*//')
             value=$(echo $1 | sed 's/[^=]*=//')
-            echo export $key="$value" >> $ENV_FILE
+            sudo echo export $key="$value" >> $ENV_FILE
             ;;
         --*) 
             key=$(echo $1 | sed 's/--//')
             value=$2
-            echo export $key="$value" >> $ENV_FILE
+            sudo echo export $key="$value" >> $ENV_FILE
             shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
